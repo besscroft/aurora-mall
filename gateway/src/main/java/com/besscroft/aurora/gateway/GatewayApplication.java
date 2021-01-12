aed0ddc3366621bb86dc1a68de62fe73833ba94e
besscroft <631908942@qq.com>
2021-01-12 16:48:21 +0800

    feat:👻项目初始化

---
+++ gateway/src/main/java/com/besscroft/aurora/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.besscroft.aurora.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
