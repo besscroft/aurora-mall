3aa4d4d783a64e0adff76381e0e1c1c69675c602
besscroft <631908942@qq.com>
2021-08-12 10:51:59 +0800

    doc:update aurora-mall.sql

--- doc/sql/aurora-mall.sql
+++ doc/sql/aurora-mall.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50734
  File Encoding         : 65001
 
- Date: 12/08/2021 09:53:23
+ Date: 12/08/2021 10:50:37
 */
 
 SET NAMES utf8mb4;
@@ -3119,39 +3119,40 @@ CREATE TABLE `bms_auth_menu`  (
   `hidden` int(1) NULL DEFAULT 1 COMMENT '显示状态：0->不显示；1->显示',
   `component` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '组件路径',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 52 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '权限管理模块菜单表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 101 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '权限管理模块菜单表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of bms_auth_menu
 -- ----------------------------
-INSERT INTO `bms_auth_menu` VALUES (1, 0, '2021-03-21 18:56:44', '权限管理', NULL, 1, 1, 'auth', '/auth', 'el-icon-success', 0, 'Layout');
-INSERT INTO `bms_auth_menu` VALUES (2, 0, '2021-03-21 18:57:06', '订单管理', NULL, 1, 2, 'order', '/order', 'el-icon-s-order', 0, 'Layout');
-INSERT INTO `bms_auth_menu` VALUES (3, 0, '2021-03-21 18:57:49', '商品管理', NULL, 1, 3, 'product', '/product', 'el-icon-s-goods', 0, 'Layout');
-INSERT INTO `bms_auth_menu` VALUES (4, 0, '2021-03-30 10:47:44', '营销管理', NULL, 1, 4, 'market', '/market', 'el-icon-s-marketing', 0, 'Layout');
-INSERT INTO `bms_auth_menu` VALUES (5, 0, '2021-03-30 10:48:47', '会员管理', NULL, 1, 5, 'user', '/user', 'el-icon-user-solid', 0, 'Layout');
-INSERT INTO `bms_auth_menu` VALUES (6, 0, '2021-03-30 10:49:29', '系统管理', NULL, 1, 6, 'system', '/system', 'el-icon-s-tools', 0, 'Layout');
-INSERT INTO `bms_auth_menu` VALUES (10, 1, '2021-03-21 18:57:47', '用户管理', '权限管理', 2, 0, 'authUser', '/auth/authUser', 'el-icon-user-solid', 0, '/auth/authUser/index');
-INSERT INTO `bms_auth_menu` VALUES (11, 1, '2021-03-21 18:58:31', '角色管理', '权限管理', 2, 0, 'authRole', '/auth/authRole', 'el-icon-s-custom', 0, '/auth/authRole/index');
-INSERT INTO `bms_auth_menu` VALUES (12, 1, '2021-03-21 18:58:34', '菜单管理', '权限管理', 2, 0, 'authMenu', '/auth/authMenu', 'el-icon-menu', 0, '/auth/authMenu/index');
-INSERT INTO `bms_auth_menu` VALUES (13, 1, '2021-03-30 10:41:52', '资源管理', '权限管理', 2, 0, 'authResource', '/auth/authResource', 'el-icon-s-promotion', 0, '/auth/authResource/index');
-INSERT INTO `bms_auth_menu` VALUES (14, 1, '2021-03-30 10:44:02', '权限管理', '权限管理', 2, 0, 'authPermission', '/auth/authPermission', 'el-icon-check', 0, '/auth/authPermission/index');
-INSERT INTO `bms_auth_menu` VALUES (15, 1, '2021-03-30 10:46:32', '资源类别管理', '权限管理', 2, 0, 'authResourceSort', '/auth/authResourceSort', 'el-icon-finished', 0, '/auth/authResourceSort/index');
-INSERT INTO `bms_auth_menu` VALUES (20, 2, '2021-03-30 10:36:21', '订单列表', '订单管理', 2, 0, 'orderList', '/order/orderList', 'el-icon-s-order', 0, '/order/orderList/index');
-INSERT INTO `bms_auth_menu` VALUES (21, 2, '2021-03-30 10:38:40', '定时任务', '订单管理', 2, 0, 'orderTimeTask', '/order/orderTimeTask', 'el-icon-timer', 0, '/order/orderTimeTask/index');
-INSERT INTO `bms_auth_menu` VALUES (22, 2, '2021-03-30 10:39:58', '售后订单', '订单管理', 2, 0, 'orderReturn', '/order/orderReturn', 'el-icon-s-claim', 0, '/order/orderReturn');
-INSERT INTO `bms_auth_menu` VALUES (23, 2, '2021-03-30 10:40:43', '售后原因', '订单管理', 2, 0, 'orderReason', '/order/orderReason', 'el-icon-notebook-1', 0, '/order/orderReason/index');
-INSERT INTO `bms_auth_menu` VALUES (30, 3, '2021-03-30 10:33:46', '品牌管理', '商品管理', 2, 0, 'productBrand', '/product/productBrand', 'el-icon-postcard', 0, '/product/productBrand/index');
-INSERT INTO `bms_auth_menu` VALUES (31, 3, '2021-03-27 15:47:53', '商品列表', '商品管理', 2, 0, 'productList', '/product/productList', 'el-icon-s-goods', 0, '/product/productList/index');
-INSERT INTO `bms_auth_menu` VALUES (32, 3, '2021-03-30 10:31:03', '类型管理', '商品管理', 2, 0, 'productType', '/product/productType', 'el-icon-price-tag', 0, '/product/productType/index');
-INSERT INTO `bms_auth_menu` VALUES (33, 3, '2021-03-30 10:29:15', '分类管理', '商品管理', 2, 0, 'productSort', '/product/productSort', 'el-icon-discount', 0, '/product/productSort/index');
-INSERT INTO `bms_auth_menu` VALUES (34, 3, '2021-03-27 15:49:10', '修改商品', '商品管理', 2, 0, 'productUpdate', '/product/productUpdate', 'el-icon-sold-out', 0, '/product/productUpdate/index');
-INSERT INTO `bms_auth_menu` VALUES (35, 3, '2021-03-27 15:49:04', '添加商品', '商品管理', 2, 0, 'productAdd', '/product/productAdd', 'el-icon-sell', 0, '/product/productAdd/index');
-INSERT INTO `bms_auth_menu` VALUES (40, 4, '2021-03-30 10:54:02', '虚拟币管理', '营销管理', 2, 0, 'marketBit', '/market/marketBit', 'el-icon-s-finance', 0, '/market/marketBit/index');
-INSERT INTO `bms_auth_menu` VALUES (41, 4, '2021-03-30 10:51:23', '秒杀管理', '营销管理', 2, 0, 'marketSpike', '/market/marketSpike', 'el-icon-alarm-clock', 0, '/market/marketSpike/index');
-INSERT INTO `bms_auth_menu` VALUES (42, 4, '2021-03-30 10:52:29', '优惠券管理', '营销管理', 2, 0, 'marketCoupon', '/market/marketCoupon', 'el-icon-s-ticket', 0, '/market/marketCoupon/index');
-INSERT INTO `bms_auth_menu` VALUES (43, 4, '2021-03-30 10:53:21', '广告管理', '营销管理', 2, 0, 'marketAD', '/market/marketAD', 'el-icon-data-line', 0, '/market/marketAD/index');
-INSERT INTO `bms_auth_menu` VALUES (50, 5, '2021-03-30 10:55:06', '会员列表', '会员管理', 2, 0, 'userList', '/user/userList', 'el-icon-user', 0, '/user/userList/index');
-INSERT INTO `bms_auth_menu` VALUES (51, 6, '2021-04-10 10:39:32', '版本日志', '系统管理', 2, 0, 'version', '/system/version', 'el-icon-s-promotion', 0, '/system/version/index');
+INSERT INTO `bms_auth_menu` VALUES (1, 0, '2021-03-21 18:56:44', '权限管理', NULL, 1, 1, 'auth', '/auth', 'el-icon-success', 1, 'Layout');
+INSERT INTO `bms_auth_menu` VALUES (2, 0, '2021-03-21 18:57:06', '订单管理', NULL, 1, 2, 'order', '/order', 'el-icon-s-order', 1, 'Layout');
+INSERT INTO `bms_auth_menu` VALUES (3, 0, '2021-03-21 18:57:49', '商品管理', NULL, 1, 3, 'product', '/product', 'el-icon-s-goods', 1, 'Layout');
+INSERT INTO `bms_auth_menu` VALUES (4, 0, '2021-03-30 10:47:44', '营销管理', NULL, 1, 4, 'market', '/market', 'el-icon-s-marketing', 1, 'Layout');
+INSERT INTO `bms_auth_menu` VALUES (5, 0, '2021-03-30 10:48:47', '会员管理', NULL, 1, 5, 'user', '/user', 'el-icon-user-solid', 1, 'Layout');
+INSERT INTO `bms_auth_menu` VALUES (6, 0, '2021-03-30 10:49:29', '系统管理', NULL, 1, 6, 'system', '/system', 'el-icon-s-tools', 1, 'Layout');
+INSERT INTO `bms_auth_menu` VALUES (10, 1, '2021-03-21 18:57:47', '用户管理', '权限管理', 2, 0, 'authUser', '/auth/authUser', 'el-icon-user-solid', 1, '/auth/authUser/index');
+INSERT INTO `bms_auth_menu` VALUES (11, 1, '2021-03-21 18:58:31', '角色管理', '权限管理', 2, 0, 'authRole', '/auth/authRole', 'el-icon-s-custom', 1, '/auth/authRole/index');
+INSERT INTO `bms_auth_menu` VALUES (12, 1, '2021-03-21 18:58:34', '菜单管理', '权限管理', 2, 0, 'authMenu', '/auth/authMenu', 'el-icon-menu', 1, '/auth/authMenu/index');
+INSERT INTO `bms_auth_menu` VALUES (13, 1, '2021-03-30 10:41:52', '资源管理', '权限管理', 2, 0, 'authResource', '/auth/authResource', 'el-icon-s-promotion', 1, '/auth/authResource/index');
+INSERT INTO `bms_auth_menu` VALUES (14, 1, '2021-03-30 10:44:02', '权限管理', '权限管理', 2, 0, 'authPermission', '/auth/authPermission', 'el-icon-check', 1, '/auth/authPermission/index');
+INSERT INTO `bms_auth_menu` VALUES (15, 1, '2021-03-30 10:46:32', '资源类别管理', '权限管理', 2, 0, 'authResourceSort', '/auth/authResourceSort', 'el-icon-finished', 1, '/auth/authResourceSort/index');
+INSERT INTO `bms_auth_menu` VALUES (20, 2, '2021-03-30 10:36:21', '订单列表', '订单管理', 2, 0, 'orderList', '/order/orderList', 'el-icon-s-order', 1, '/order/orderList/index');
+INSERT INTO `bms_auth_menu` VALUES (21, 2, '2021-03-30 10:38:40', '定时任务', '订单管理', 2, 0, 'orderTimeTask', '/order/orderTimeTask', 'el-icon-timer', 1, '/order/orderTimeTask/index');
+INSERT INTO `bms_auth_menu` VALUES (22, 2, '2021-03-30 10:39:58', '售后订单', '订单管理', 2, 0, 'orderReturn', '/order/orderReturn', 'el-icon-s-claim', 1, '/order/orderReturn');
+INSERT INTO `bms_auth_menu` VALUES (23, 2, '2021-03-30 10:40:43', '售后原因', '订单管理', 2, 0, 'orderReason', '/order/orderReason', 'el-icon-notebook-1', 1, '/order/orderReason/index');
+INSERT INTO `bms_auth_menu` VALUES (30, 3, '2021-03-30 10:33:46', '品牌管理', '商品管理', 2, 0, 'productBrand', '/product/productBrand', 'el-icon-postcard', 1, '/product/productBrand/index');
+INSERT INTO `bms_auth_menu` VALUES (31, 3, '2021-03-27 15:47:53', '商品列表', '商品管理', 2, 0, 'productList', '/product/productList', 'el-icon-s-goods', 1, '/product/productList/index');
+INSERT INTO `bms_auth_menu` VALUES (32, 3, '2021-03-30 10:31:03', '类型管理', '商品管理', 2, 0, 'productType', '/product/productType', 'el-icon-price-tag', 1, '/product/productType/index');
+INSERT INTO `bms_auth_menu` VALUES (33, 3, '2021-03-30 10:29:15', '分类管理', '商品管理', 2, 0, 'productSort', '/product/productSort', 'el-icon-discount', 1, '/product/productSort/index');
+INSERT INTO `bms_auth_menu` VALUES (34, 3, '2021-03-27 15:49:10', '修改商品', '商品管理', 2, 0, 'productUpdate', '/product/productUpdate', 'el-icon-sold-out', 1, '/product/productUpdate/index');
+INSERT INTO `bms_auth_menu` VALUES (35, 3, '2021-03-27 15:49:04', '添加商品', '商品管理', 2, 0, 'productAdd', '/product/productAdd', 'el-icon-sell', 1, '/product/productAdd/index');
+INSERT INTO `bms_auth_menu` VALUES (40, 4, '2021-03-30 10:54:02', '虚拟币管理', '营销管理', 2, 0, 'marketBit', '/market/marketBit', 'el-icon-s-finance', 1, '/market/marketBit/index');
+INSERT INTO `bms_auth_menu` VALUES (41, 4, '2021-03-30 10:51:23', '秒杀管理', '营销管理', 2, 0, 'marketSpike', '/market/marketSpike', 'el-icon-alarm-clock', 1, '/market/marketSpike/index');
+INSERT INTO `bms_auth_menu` VALUES (42, 4, '2021-03-30 10:52:29', '优惠券管理', '营销管理', 2, 0, 'marketCoupon', '/market/marketCoupon', 'el-icon-s-ticket', 1, '/market/marketCoupon/index');
+INSERT INTO `bms_auth_menu` VALUES (43, 4, '2021-03-30 10:53:21', '广告管理', '营销管理', 2, 0, 'marketAD', '/market/marketAD', 'el-icon-data-line', 1, '/market/marketAD/index');
+INSERT INTO `bms_auth_menu` VALUES (50, 5, '2021-03-30 10:55:06', '会员列表', '会员管理', 2, 0, 'userList', '/user/userList', 'el-icon-user', 1, '/user/userList/index');
+INSERT INTO `bms_auth_menu` VALUES (51, 6, '2021-04-10 10:39:32', '版本日志', '系统管理', 2, 0, 'version', '/system/version', 'el-icon-s-promotion', 1, '/system/version/index');
+INSERT INTO `bms_auth_menu` VALUES (100, 0, '2021-08-12 10:30:46', '首页', NULL, 1, 0, 'Dashboard', '/dashboard', 'el-icon-s-platform', 1, 'Layout');
 
 -- ----------------------------
 -- Table structure for bms_auth_permission
@@ -3327,60 +3328,55 @@ CREATE TABLE `bms_auth_role_menu_relation`  (
   `role_id` bigint(20) NULL DEFAULT NULL COMMENT '角色ID',
   `menu_id` bigint(20) NULL DEFAULT NULL COMMENT '菜单ID',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 91 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '权限管理模块角色菜单关系表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 178 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '权限管理模块角色菜单关系表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of bms_auth_role_menu_relation
 -- ----------------------------
-INSERT INTO `bms_auth_role_menu_relation` VALUES (1, 1, 1);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (2, 1, 2);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (3, 1, 3);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (4, 1, 4);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (5, 1, 5);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (6, 1, 6);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (8, 1, 10);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (9, 1, 11);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (10, 1, 12);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (11, 1, 13);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (12, 1, 14);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (13, 1, 15);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (14, 1, 20);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (15, 1, 21);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (16, 1, 22);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (17, 1, 23);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (18, 1, 30);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (19, 1, 31);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (20, 1, 32);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (21, 1, 33);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (22, 1, 34);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (23, 1, 35);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (24, 1, 40);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (25, 1, 41);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (26, 1, 42);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (27, 1, 43);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (28, 1, 50);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (42, 1, 51);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (70, 2, 1);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (71, 2, 10);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (72, 2, 11);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (73, 2, 12);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (74, 2, 13);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (75, 2, 14);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (76, 2, 15);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (77, 2, 2);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (78, 2, 20);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (79, 2, 21);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (80, 2, 22);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (81, 2, 23);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (82, 2, 30);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (83, 2, 31);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (84, 2, 32);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (85, 2, 33);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (86, 2, 43);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (87, 2, 5);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (88, 2, 50);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (89, 2, 6);
-INSERT INTO `bms_auth_role_menu_relation` VALUES (90, 2, 51);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (112, 1, 1);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (113, 1, 10);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (114, 1, 11);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (115, 1, 12);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (116, 1, 13);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (117, 1, 14);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (118, 1, 15);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (119, 1, 2);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (120, 1, 20);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (121, 1, 21);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (122, 1, 22);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (123, 1, 23);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (124, 1, 3);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (125, 1, 30);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (126, 1, 31);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (127, 1, 32);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (128, 1, 33);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (129, 1, 34);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (130, 1, 35);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (131, 1, 4);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (132, 1, 40);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (133, 1, 41);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (134, 1, 42);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (135, 1, 43);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (136, 1, 5);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (137, 1, 50);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (138, 1, 6);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (139, 1, 51);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (140, 1, 100);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (163, 2, 2);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (164, 2, 20);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (165, 2, 21);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (166, 2, 22);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (167, 2, 23);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (168, 2, 30);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (169, 2, 31);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (170, 2, 32);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (171, 2, 33);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (172, 2, 43);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (173, 2, 5);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (174, 2, 50);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (175, 2, 6);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (176, 2, 51);
+INSERT INTO `bms_auth_role_menu_relation` VALUES (177, 2, 100);
 
 -- ----------------------------
 -- Table structure for bms_auth_role_permission_relation
@@ -3502,7 +3498,7 @@ CREATE TABLE `bms_auth_user`  (
 -- ----------------------------
 -- Records of bms_auth_user
 -- ----------------------------
-INSERT INTO `bms_auth_user` VALUES (1, 'admin', '$2a$10$E0A60hzJ.yBHJhyZ970Oze205OGuu4LIrjDSPQvcGBDl40O0oaiqC', 'https://www.52bess.com/uploads/avatar.png', 'admin@qq.com', '12345678901', '管理员', '管理员', '2021-02-24 21:22:48', '2021-07-23 22:24:49', 1, 1);
+INSERT INTO `bms_auth_user` VALUES (1, 'admin', '$2a$10$E0A60hzJ.yBHJhyZ970Oze205OGuu4LIrjDSPQvcGBDl40O0oaiqC', 'https://www.52bess.com/uploads/avatar.png', 'admin@qq.com', '12345678901', '管理员', '管理员', '2021-02-24 21:22:48', '2021-08-12 10:50:12', 1, 1);
 INSERT INTO `bms_auth_user` VALUES (2, 'test', '$2a$10$E0A60hzJ.yBHJhyZ970Oze205OGuu4LIrjDSPQvcGBDl40O0oaiqC', 'https://www.52bess.com/uploads/avatar.png', 'test@qq.com', '12345678902', '测试员', '测试员', '2021-03-21 13:42:10', '2021-04-24 12:45:54', 1, 1);
 INSERT INTO `bms_auth_user` VALUES (3, 'user1', '$2a$10$U9qlXI22XmUjzAgZiH0kMOalOkBTM23LvmAownM1GNXNQTRhO4Mtu', 'https://www.52bess.com/uploads/avatar.png', 'user1@qq.com', '111', '普通用户1', '普通用户1', '2021-04-04 18:18:44', '2021-04-04 18:18:44', 1, 1);
 INSERT INTO `bms_auth_user` VALUES (4, 'user2', '$2a$10$qbw14yY1.sHYGb/dytzdSeSSR78KXjoHG3XQImP/iqh7iGjCc.TZK', 'https://www.52bess.com/uploads/avatar.png', 'user2@qq.com', '112', '普通用户2', '普通用户2', '2021-04-04 18:37:01', '2021-04-04 18:37:01', 1, 1);
