2bcd997c673be09c9ac86077e866f03d26e5a3d6
besscroft <besscroft@foxmail.com>
2022-01-16 16:33:30 +0800

    feat:新增访问日志查看  - 新增访问日志列表接口  - 添加 log 服务单元测试

--- mall-admin/admin-api/src/main/java/com/besscroft/aurora/mall/admin/api/LogFeignClient.java
+++ mall-admin/admin-api/src/main/java/com/besscroft/aurora/mall/admin/api/LogFeignClient.java
@@ -4,8 +4,10 @@ import com.besscroft.aurora.mall.admin.api.fallback.LogFeignFallbackClient;
 import com.besscroft.aurora.mall.common.entity.WebLog;
 import com.besscroft.aurora.mall.common.result.AjaxResult;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @Description 日志服务
@@ -22,5 +24,10 @@ public interface LogFeignClient {
      */
     @PostMapping(value = "/log/mallLog")
     AjaxResult mallLog(@RequestBody WebLog webLog);
+
+    @GetMapping(value = "/log/list")
+    AjaxResult getWebLogList(@RequestParam(value = "pageNum") Integer pageNum,
+                             @RequestParam(value = "pageSize") Integer pageSize,
+                             @RequestParam(value = "keyword") String keyword);
 
 }
--- mall-log/src/main/java/com/besscroft/aurora/mall/log/service/LogService.java
+++ mall-log/src/main/java/com/besscroft/aurora/mall/log/service/LogService.java
@@ -1,6 +1,9 @@
 package com.besscroft.aurora.mall.log.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.besscroft.aurora.mall.common.entity.WebLog;
+
+import java.util.List;
 
 /**
  * 日志服务
@@ -8,7 +11,7 @@ import com.besscroft.aurora.mall.common.entity.WebLog;
  * @Author Bess Croft
  * @Time 2021/7/18 17:55
  */
-public interface LogService {
+public interface LogService extends IService<WebLog> {
 
     /**
      * 保存web日志
@@ -16,5 +19,14 @@ public interface LogService {
      * @return
      */
     boolean saveWebLog(WebLog webLog);
+
+    /**
+     * 分页查询访问日志
+     * @param pageNum 页码
+     * @param pageSize 页大小
+     * @param keyword 关键字
+     * @return
+     */
+    List<WebLog> pageList(Integer pageNum, Integer pageSize, String keyword);
 
 }
